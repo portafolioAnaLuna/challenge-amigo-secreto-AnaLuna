4ef2381f45c580842dd9f0bb1c830747184e0324
portafolioAnaLuna <ana.lunamed26@gmail.com>
2025-03-18 14:20:22 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,27 +6,21 @@ const botonAñadir = document.querySelector(".button-add");
 
 escribirAmigos.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        agregarAmigo(); // Ejecutar la función al presionar Enter
+        agregarAmigo();
     }
 });
 
 function agregarAmigo(){
     const amigo = escribirAmigos.value.trim();
-    
-    // Validación: asegurarse de que sea un string y no esté vacío
     if (amigo === "") {
         alert("Por favor, ingrese un nombre válido.");
-        return; // Detener la función si el campo está vacío
+        return;
     }
-
-    // Validación: asegurarse de que el nombre no sea un número
     if (!isNaN(amigo)) {
         alert("El nombre no puede ser un número.");
         limpiarCaja();
         return;
     };
-    
-    // Validación: asegurarse de que el nombre no esté repetido
     if (listaAmigos.includes(amigo)) {
         alert("Este nombre ya fue añadido.");
         limpiarCaja();
@@ -34,8 +28,6 @@ function agregarAmigo(){
     };
     listaAmigos.push(escribirAmigos.value);
     creandoListaAmigos.innerHTML = creandoListaAmigos.innerHTML + `<li>${escribirAmigos.value}</li>`;
-    // creandoListaAmigos.innerHTML += `<li>${inputAmigos.value}</li>`; 
-    //es lo mismo que lo de arriba, pero de una forma abrebiada
     limpiarCaja();
 };
 
